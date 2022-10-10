90348bc53c028b3c02b56b0d7623b9ebb877e387
Administrador <Administrador@DESKTOP-HQ1VCUM>
2022-10-10 18:15:38 +0200

    Commit 1 Branch

--- EjemploJava/src/paquete/HolaMundo.java
+++ EjemploJava/src/paquete/HolaMundo.java
@@ -3,6 +3,6 @@ package paquete;
 public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
-		System.out.println("4 merge");
+		System.out.println("1 BRACH");
 	}
 }
